d7f0acf2c7a4cc9f76227763c94c4bb513332852
Ye Kyaw Thu <yktnlp@gmail.com>
2021-06-28 14:54:04 +0700

    Add files via upload

---
+++ corpus-ver-3.0/iSAI-NLP2020-paper-experiment/2_CRF/2_CRF_with_otest_100%_ASEAN_Corpus/ch2col-4all.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# last updated 16 May 2017
+# written by Ye Kyaw Thu
+# changing line to column format for all training and test data files
+
+#for i in {1..10}
+for i in {1..1}
+do
+   # for training files
+   ./ch2col.pl ./t$i/train$i.nopipe > ./t$i/train$i.nopipe.col
+   echo -e "\nFinished for ./t$i/train$i.nopipe!"
+   echo "head ./t$i/train$i.nopipe.col"
+   head ./t$i/train$i.nopipe.col
+
+   # for closed-test data files
+   ./ch2col.pl ./t$i/ctest$i.nopipe > ./t$i/ctest$i.nopipe.col
+   echo -e "\nFinished for ./t$i/ctest$i.nopipe!"
+   echo "head ./t$i/ctest$i.nopipe.col"
+   head ./t$i/ctest$i.nopipe.col
+
+done 
+   
+   ./ch2col.pl ./otest.nopipe > ./otest.nopipe.col #for open test data
+   echo -e "\nFinished for open test data!"
+   echo "head otest.nopipe.col"
+   head ./otest.nopipe.col
+
---
+++ corpus-ver-3.0/iSAI-NLP2020-paper-experiment/2_CRF/2_CRF_with_otest_100%_ASEAN_Corpus/cut-f1and3-all.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# late update 16 May 2017
+# Written by Ye Kyaw Thu
+# for cutting field 1 and field 3 (i.e. tagged by CRF model) for evaluation
+
+#for i in {1..10}
+for i in {1..1}
+do
+   cut -f1,3 ./t$i/ctest$i.nopipe.col.result > ./t$i/ctest$i.nopipe.col.result.f13
+   cut -f1,3 ./t$i/otest$i.nopipe.col.result > ./t$i/otest$i.nopipe.col.result.f13
+
+   echo -e "\n Finished testing for ./t$i/ !"
+done
